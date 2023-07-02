205872134d97065ccb93ad27081fc5e79b853090
eosti <reidsox1@gmail.com>
2023-07-02 13:13:12 -0400

    bundle font for consistency

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
         fontFamily: {
-            'display': ['Helvetica Neue', "HelveticaNeue", "TeX Gyre Heros", "TeXGyreHeros", "FreeSans", "Nimbus Sans L", "Liberation Sans", "Arimo", "Helvetica", "Arial", "sans-serif"],
+            //'display': ['Helvetica Neue', "HelveticaNeue", "TeX Gyre Heros", "TeXGyreHeros", "FreeSans", "Nimbus Sans L", "Liberation Sans", "Arimo", "Helvetica", "Arial", "sans-serif"],
+            'display': ['texgyreheros'],
             'body': ['Source Sans Pro'],
         },
 		extend: {},
