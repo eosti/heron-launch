ecd2bb8ffc6fbedd00604f56cc981491f20e4d3d
eosti <reidsox1@gmail.com>
2023-07-02 01:10:03 -0400

    actually, move to netlify instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: 'https://hasheronlaunchedyet.com'
+    site: 'https://hasheronlaunchedyet.com',
     
     experimental: {
         redirects: true
